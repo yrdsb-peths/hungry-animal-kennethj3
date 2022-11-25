92ee523d8df4a11edf77952ba29ba2dc3cc90f63
kennethj3 <349320036@gapps.yrdsb.ca>
2022-11-24 21:59:02 -0500

    Accepted the assignment

--- MyWorld.java
+++ MyWorld.java
@@ -13,7 +13,7 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
+    public MyWorld() 
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
