f9b67da248e3c6dfe6f9a3bea938fca828b00a7d
kennethj3 <349320036@gapps.yrdsb.ca>
2022-11-30 13:54:30 -0500

    Random apple

--- MyWorld.java
+++ MyWorld.java
@@ -20,7 +20,14 @@ public class MyWorld extends World
         Elephant elephant = new Elephant();
         addObject(elephant, 100, 300);
         
+        spawnApple();
+    }
+    
+    public void spawnApple()
+    {
         Apple apple = new Apple();
-        addObject(apple, 300, 10);
+        int x = Greenfoot.getRandomNumber(getWidth());
+        int y = 10;
+        addObject(apple, x, y);
     }
 }
