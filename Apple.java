c3e8b7025ae0c46a7b5a28a75abf088b63babd54
kennethj3 <349320036@gapps.yrdsb.ca>
2022-12-05 18:20:30 -0500

    Game over label

--- Apple.java
+++ Apple.java
@@ -14,8 +14,17 @@ public class Apple extends Actor
      */
     public void act()
     {
-        setRotation(90);
-        move(1);
+        int x = getX();
+        int y = getY() + 2;
+        setLocation(x, y);
+        
+        MyWorld world = (MyWorld) getWorld();
+        
+        if(getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
         // Add your action code here.
     }
 }
